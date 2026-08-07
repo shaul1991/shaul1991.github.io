c56e106eb237d9da17045141b366dc548a99785e
atms-jihoon <jihoon@atmsads.io>
2026-08-07 22:59:58 +0900

    수정: 운영 페이지 스타일을 HTML에 포함

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://shaul1991.github.io',
   output: 'static',
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
